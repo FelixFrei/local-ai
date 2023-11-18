f42fd74d1540913958facc25b974175bc6ceb6b5
Felix Frei <felix.frei@gmx.ch>
2023-11-18 08:38:05 +0100

    ensure requirements is setup

--- setenv.py
+++ setenv.py
@@ -248,6 +248,9 @@ def update_requirements(initial_installation=False):
     run_cmd("python -m pip install -r temp_requirements.txt --upgrade", assert_success=True, environment=True)
     os.remove('temp_requirements.txt')
 
+    # Install/update the project requirements
+    run_cmd("python -m pip install -r requirements.txt --upgrade", assert_success=True, environment=True)
+
     # Check for '+cu' or '+rocm' in version string to determine if torch uses CUDA or ROCm. Check for pytorch-cuda as well for backwards compatibility
     if not any((is_cuda, is_rocm)) and run_cmd("conda list -f pytorch-cuda | grep pytorch-cuda", environment=True, capture_output=True).returncode == 1:
         clear_cache()
